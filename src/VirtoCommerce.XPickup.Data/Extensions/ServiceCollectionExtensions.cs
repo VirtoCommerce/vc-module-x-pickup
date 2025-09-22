f551d7f664aa3d0fbbc36500629acd4b59d4cc04
yuskithedeveloper <yuskianywhere@gmail.com>
2025-09-22 14:44:51 -0500

    ProductPickupLocationService

--- src/VirtoCommerce.XPickup.Data/Extensions/ServiceCollectionExtensions.cs
+++ src/VirtoCommerce.XPickup.Data/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 using GraphQL.DI;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Xapi.Core.Infrastructure;
+using VirtoCommerce.XPickup.Core.Services;
+using VirtoCommerce.XPickup.Data.Services;
 
 namespace VirtoCommerce.XPickup.Data.Extensions;
 
@@ -9,6 +11,8 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddXPickup(this IServiceCollection services, IGraphQLBuilder graphQLBuilder)
     {
         services.AddSingleton<ScopedSchemaFactory<DataAssemblyMarker>>();
+
+        services.AddTransient<IProductPickupLocationService, ProductPickupLocationService>();
 
         return services;
     }
